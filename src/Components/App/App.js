cd3e710a7248342ee67b4cb0587974bf1d8a0633
Sheehan1512 <sheehanvy@gmail.com>
2022-01-19 17:42:10 +0600

    navbar

---
+++ src/Components/App/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import React from 'react';
+import NavBar from './NavBar/NavBar';
+
+function App() {
+  return(
+    <NavBar />
+  );
+}
+
+export default App;
---
+++ src/Components/App/NavBar/NavBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './NavBar.css';
+
+function NavBar(props) {
+    return(
+        <nav className="navbar">
+            <ul className="navbar-nav">
+                
+            </ul>
+        </nav>
+    );
+}
+
+export default NavBar;
