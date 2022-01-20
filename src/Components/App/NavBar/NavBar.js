fe5f33db387b78e6efbe7999012a9afe8348230a
Sheehan1512 <sheehanvy@gmail.com>
2022-01-20 11:15:25 +0600

    nav items first draft

--- src/Components/App/NavBar/NavBar.js
+++ src/Components/App/NavBar/NavBar.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 import './NavBar.css';
+import NavItem from "./NavItem/NavItem";
+
+import {ReactComponent as Arrow} from '../../../icons/arrow.svg';
+import {ReactComponent as Bell} from '../../../icons/bell.svg';
+import {ReactComponent as Bolt} from '../../../icons/bolt.svg';
+import {ReactComponent as Caret} from '../../../icons/caret.svg';
+import {ReactComponent as Chevron} from '../../../icons/chevron.svg';
+import {ReactComponent as Cog} from '../../../icons/cog.svg';
+import {ReactComponent as Messenger} from '../../../icons/messenger.svg';
+import {ReactComponent as Plus} from '../../../icons/plus.svg';
 
 function NavBar(props) {
     return(
         <nav className="navbar">
             <ul className="navbar-nav">
-                
+
+                <NavItem icon={<Plus />} />
+                <NavItem icon={<Bell />} />
+                <NavItem icon={<Messenger />} />
+
+                <NavItem icon={<Caret />}>
+
+                </NavItem>
+
             </ul>
         </nav>
     );
